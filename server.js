a80da2334fa91807785132c78b84b89636c175d9
Nikola-Mircic <nikola123mircic@gmail.com>
2022-01-12 22:52:45 +0100

    Routing path and socket.io in separate files

--- server.js
+++ server.js
@@ -1,90 +1,52 @@
-const express = require('express');
 const socketIO = require('socket.io');
-const SocketIOFileUpload = require("socketio-file-upload");
 
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const requestIp = require('request-ip');
-
-const path = require('path');
 const fs = require('fs');
-const fileUpload = require('express-fileupload');
+
+const IORouter = require('./routing/IORouter');
+const app = require('./routing/PathRouter')(saveFile, saveLogData, __dirname);
+
 const readline = require("readline");
 
 const { networkInterfaces } = require('os');
 
-const app = express()
 const server = require('http').createServer(app);
 const io = socketIO(server, {});
 
-const PORT = process.env.PORT || 80;
-const available_adresses = getLocalAddress();
+var PORT = process.env.PORT || 80;
+var available_adresses = getLocalAddressList();
 var IP = "localhost";
 
 const LogSystem = require("./logsystem.js");
+const { diffieHellman } = require('crypto');
 const Log = new LogSystem();
 const LOG_PATH = __dirname+"/db/logs/log_history.dat";
 
-const userInCLI = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const default_ip = findValue("-default-ip");
+const default_addr_name = findValue("-default-addr-name");
+const default_port = findValue("-default-port");
 
-userInCLI.question("Select one of above networks to run your program on (to run only on your computer type 'localhost' ):", function(name) {
-    if(name === 'localhost') return;
+if(default_port !== ""){
+	PORT = parseInt(default_port);
+}
 
-    let selection = available_adresses[name];
+if(default_ip !== ""){
+	IP = default_ip;
 
-    if(selection)
-    	IP = selection[0];
-    else{
-    	IP = "localhost";
-    	console.log(`Can't find network with name: ${name}`);
-    }
-
-    userInCLI.close();
-});
-
-userInCLI.on("close", function() {
-    console.log(`Ip address set to ${IP}`);
-
-    server.listen(PORT,IP,()=>{
+	server.listen(PORT,IP,()=>{
 		console.log("Server started on http://"+IP+":"+PORT);
 	});
-});
+	IORouter(io, saveFile, saveLogData);
+}else if(default_addr_name !== ""){
+	IP = available_adresses[default_addr_name][0];
 
-var regUser = [];
+	server.listen(PORT,IP,()=>{
+		console.log("Server started on http://"+IP+":"+PORT);
+	});
+	IORouter(io, saveFile, saveLogData);
+}else{
+	inputUserAddress();
+}
 
-app.use(fileUpload({
-    createParentPath: true
-}));
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(morgan('dev'));
-app.use(requestIp.mw());
-
-app.use('/assets',express.static(path.join(__dirname,'/client/assets')));
-
-app.get('/', function(req, res, next){
-	console.log(`Sending response to the user [${requestIp.getClientIp(req)}]...`);
-	res.sendFile(__dirname+'/client/index.html');
-});
-
-app.post('/', function(req, res){
-	if(Array.isArray(req.files.file)){
-		var upload = req.files.file;
-		upload.forEach((file,idx)=>{
-			console.log(`Started saving file #${(idx+1)}...`);
-			saveFile(idx, file, requestIp.getClientIp(req));
-			saveLogData(idx, file, requestIp.getClientIp(req));
-		});
-	}else{
-		saveFile(0, req.files.file, requestIp.getClientIp(req));
-		saveLogData(0, req.files.file, requestIp.getClientIp(req));
-	}
-	
-    res.redirect('/');
-});
 
 function saveFile(fileID, file, sender, transport=false){
 	let fileName = file.name;
@@ -113,63 +75,54 @@ function saveLogData(fileID, file, sender){
 	});
 }
 
-var startTime = 0;
-io.on('connection', (socket)=>{
-	console.log("Sending data to new user");
-	regUser.forEach((item, index)=>{
-		socket.emit('newUser', item);
+function inputUserAddress(){
+	const userInCLI = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
 	});
 
-	socket.on('register',(data)=>{
-		var user = {
-			'name': data.name,
-			'id': socket.id
-		};
-
-		regUser[regUser.length] = user;
-		socket.broadcast.emit('newUser', user);
+	console.log("Available networks:");
+	Object.keys(available_adresses).map((resultName)=>{
+		console.log(`  - ${resultName}`);
 	});
 
-	socket.on("start-transfer",(data)=>{
-		var date = new Date();
-		startTime = date.getTime();
-		io.to(data.receiver).emit("start-transfer",data);
+	userInCLI.question("Select one of above networks to run your program on (to run only on your computer type 'localhost' ):", function(name) {
+		if(name === 'localhost') return;
+	
+		let selection = available_adresses[name];
+	
+		if(selection)
+			IP = selection[0];
+		else{
+			IP = "localhost";
+			console.log(`Can't find network with name: ${name}`);
+		}
+	
+		userInCLI.close();
+	});
+	
+	userInCLI.on("close", function() {
+		console.log(`Ip address set to ${IP}`);
+
+		server.listen(PORT,IP,()=>{
+			console.log("Server started on http://"+IP+":"+PORT);
+		});
+		IORouter(io, saveFile, saveLogData);
+	});
+}
+
+function findValue(label){
+	var result = "";
+
+	process.argv.forEach((value, idx)=>{
+		if(value.indexOf(label) !== -1){
+			result = process.argv[idx+1];
+		}
 	})
+	return result;
+}
 
-	socket.on('request data', (data) => { 
-		socket.to(data.from).emit('request data',data);
-	});
-
-	socket.on('data', (data)=>{
-		saveFile(`${data.from}-${data.receiver}`,
-				 data.file,
-				 data.from,
-				 transport=true);
-		io.to(data.receiver).emit('data',data);
-	});
-
-	socket.on("transfer end",(data)=>{
-		var date = new Date();
-		saveLogData(`${data.from}-${data.receiver}`,
-					data.file,
-					data.from);
-
-		console.log(`Transfer [${data.from} -> ${data.receiver}] (${Log.getTime()})[~${date.getTime()-startTime} ms] completed!!`);
-
-		io.to(data.receiver).emit("transfer end",data);
-	});
-
-	socket.on('disconnect', ()=>{
-		regUser = regUser.filter((value, idx, arr)=>{
-			return value.id != socket.id;
-		});
-		io.emit('removeUser', {
-			'id': socket.id
-		});
-	});
-});
-
-function getLocalAddress(){
+function getLocalAddressList(){
 	const nets = networkInterfaces();
 	const results = Object.create(null); 
 
@@ -183,10 +136,6 @@ function getLocalAddress(){
 	        }
 	    }
 	}
-	console.log("Available networks:");
-	Object.keys(results).map((resultName)=>{
-		console.log(`  - ${resultName}`);
-	});
 
 	return results;
 }
